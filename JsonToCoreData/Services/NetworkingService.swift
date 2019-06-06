6aca427eb6ee7c98f26b6e67590ece31921eafd1
Derodian <derodian@gmail.com>
2019-06-06 11:23:16 -0500

    getting JSON data from web

---
+++ JsonToCoreData/Services/NetworkingService.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkingService.swift
+//  JsonToCoreData
+//
+//  Created by Googoo on 6/6/19.
+//  Copyright © 2019 Derodian. All rights reserved.
+//
+
+import Foundation
+
+class NetworkingService {
+    private init() {}
+    
+    static let instance = NetworkingService()
+    
+    
+    
+}
